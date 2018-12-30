2cf8845f5bde4799f852e88779a268d2ac284725
limingyang <jlimingyang@aliyun.com>
2018-12-30 12:35:38 +0800

    jwt封装 过滤器过滤

--- constants/UserContans.go
+++ constants/UserContans.go
@@ -2,5 +2,5 @@ package constants
 
 const MSG_REGIEST_SUC = "注册成功"
 const MSG_REGIEST_FAIL = "注册失败"
-const MSG_LOGIN_FAIL = "注册失败"
-const MSG_LOGIN_SUC = "注册成功"
+const MSG_LOGIN_FAIL = "登录失败"
+const MSG_LOGIN_SUC = "登录成功"
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
+	"item1024.com/draw/constants"
 	_ "item1024.com/draw/routers"
+	"item1024.com/draw/utils"
+	"strings"
 	"time"
 )
 
@@ -35,4 +40,22 @@ func main() {
 	beego.BeeLogger.DelLogger("log")
 	beego.SetLevel(beego.LevelDebug)
 	beego.SetLogFuncCall(true)
+	beego.InsertFilter("*/", beego.BeforeExec, FilterUser)
+}
+
+var FilterUser = func(ctx *context.Context) {
+	res := utils.Response{500, constants.MSG_LOGIN_FAIL, nil}
+	msg, err := json.Marshal(res)
+	token := ctx.Request.Header.Get("auth")
+	if !strings.Contains(token, ".") {
+		if err == nil {
+			custom := utils.CheckToken(token)
+			if custom != nil {
+				ctx.Input.SetParam("userId", custom.UserId)
+				ctx.Input.SetParam("userName", custom.UserName)
+			}
+		}
+	}
+	ctx.ResponseWriter.Write(msg)
+
 }
